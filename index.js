1dca73370c7713c0cd881b93b6af3130943d9675
josepheastman <josephryan96@gmail.com>
2019-01-07 11:53:49 -0800

    added code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db.js");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+server.get("/api/users/:userid", (req, res) => {
+  const id = req.params.userid;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.listen(5000, () => console.log("server running"));
